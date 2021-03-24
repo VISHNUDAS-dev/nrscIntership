b3345bb3de65f50bb68a1c7065f3f4d5031c0f79
VISHNUDAS-dev <78227497+VISHNUDAS-dev@users.noreply.github.com>
2021-03-24 13:23:29 +0530

    rearrange

--- assets/js/homefn.js
+++ assets/js/homefn.js
@@ -1,4 +1,7 @@
 window.addEventListener('scroll',function(){
     let nav=document.querySelector('nav');
     nav.classList.toggle('scrolling-active', window.scrollY>0);
-})
+});
+document.getElementById('upimg').addEventListener('click',function(){
+    document.querySelector('.modalbg').style.display="flex";
+});
