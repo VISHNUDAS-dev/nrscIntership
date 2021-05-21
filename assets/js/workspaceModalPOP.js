0aefadb1f8c2cf84b763d9a2608e0160a6a4ca7e
VISHNUDAS-dev <vishnudasmcapu@gmail.com>
2021-05-21 12:45:27 -0700

    may21

--- assets/js/workspaceModalPOP.js
+++ assets/js/workspaceModalPOP.js
@@ -10,5 +10,20 @@ document.getElementById('ac').addEventListener('click',function(){
 document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.modalbg').style.display="none";
     document.querySelector('body').style.overflow="visible";
+    document.getElementById('cat').value="";
+
     
 });
+let input=document.getElementById('cat');
+let button=document.getElementById('newcat');
+button.disabled=true;
+input.addEventListener("keyup",stateHandle);
+function stateHandle()
+{
+    if(document.getElementById('cat').value===""){
+        button.disabled=true;
+    }else
+    {
+        button.disabled=false;
+    }
+}
